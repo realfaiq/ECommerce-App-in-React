91fee3a1221b680e445f3d705c01c284f00afb78
Faiq Ahmad <ahmadfaiq46@gmail.com>
2022-10-01 21:18:11 +0500

    Frontend 1.0

---
+++ backend/data/products.js
@@ -0,0 +1,87 @@
+const products = [
+  {
+    _id: "1",
+    name: "Iphone 14",
+    image: "/images/Iphone14.jpg",
+    description:
+      "Looks identical to the iPhone 13, but there's a new 6.7-inch model called the iPhone 14 Plus. Under-the-hood improvements include 6GB of RAM, a 5-core GPU, Bluetooth 5.3, and multiple camera updates.",
+    brand: "Apple",
+    category: "Electronics",
+    price: 500.23,
+    countInStock: 12,
+    rating: 3.9,
+    numReviews: 5,
+  },
+
+  {
+    _id: "2",
+    name: "Samsung S9+",
+    image: "/images/S9+.jpg",
+    description:
+      "Various connectivity options on the Samsung Galaxy S9 Plus include WiFi - Yes Wi-Fi 802.11, a/ac/b/g/n/n 5GHz, MIMO, Wi-Fi Direct, Mobile Hotspot, Bluetooth - Yes v5. 0, and 4G (supports Indian bands), 3G, 2G.",
+    brand: "Samsung",
+    category: "Electronics",
+    price: 487.87,
+    countInStock: 39,
+    rating: 4.6,
+    numReviews: 7,
+  },
+
+  {
+    _id: "3",
+    name: "HTC Quad 2",
+    image: "/images/htc.jpg",
+    description:
+      "The HTC Desire 610 has Wi-Fi 802.11 b/g/n, Bluetooth 4.0, GPS and microUSB. It also has DLNA support, enabling wireless streaming from compatible devices. The phone supports 4G LTE networks.",
+    brand: "HTC",
+    category: "Electronics",
+    price: 298.65,
+    countInStock: 22,
+    rating: 3.5,
+    numReviews: 4,
+  },
+
+  {
+    _id: "4",
+    name: "Oppo Reno 22",
+    image: "/images/Reno22.jpg",
+    description:
+      "Various connectivity options on the Oppon Reno 22 Plus include WiFi - Yes Wi-Fi 802.11, a/ac/b/g/n/n 5GHz, MIMO, Wi-Fi Direct, Mobile Hotspot, Bluetooth - Yes v5. 0, and 4G (supports Indian bands), 3G, 2G.",
+    brand: "Oppo",
+    category: "Electronics",
+    price: 350.73,
+    countInStock: 36,
+    rating: 4.1,
+    numReviews: 9,
+  },
+
+  {
+    _id: "5",
+    name: "Infinix Note 12 5G",
+    image: "/images/InfinixNote12.jpg",
+    description:
+      "The Infinix Note 12 5G has Wi-Fi 802.11 b/g/n, Bluetooth 4.0, GPS and microUSB. It also has DLNA support, enabling wireless streaming from compatible devices. The phone supports 4G LTE networks.",
+    brand: "Infinix",
+    category: "Electronics",
+    price: 400.0,
+    countInStock: 9,
+    rating: 4.3,
+    numReviews: 10,
+  },
+
+  {
+    _id: "6",
+    name: "Xiaomi Mi 11",
+    image: "/images/Mi11.jpg",
+    description:
+      "The Xiaomi Mi 11 has Wi-Fi 802.11 b/g/n, Bluetooth 4.0, GPS and microUSB. It also has DLNA support, enabling wireless streaming from compatible devices. The phone supports 4G LTE networks.",
+    brand: "Xiaomi",
+    category: "Electronics",
+    price: 405.35,
+    countInStock: 15,
+    rating: 4.0,
+    numReviews: 8,
+  },
+];
+
+export default products;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,26 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
 function App() {
-  return <div>Welcome to Proshop</div>;
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
